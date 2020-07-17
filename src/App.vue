7a08a05aa1fb07e5a0162b6f51f6ee168a140a47
Eungyu Park <eungyupark@Eungyus-MacBook-Pro.local>
2020-07-17 12:51:25 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <router-view
+      :cart="cart"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      :sliderStatus="sliderStatus"
+      :maximum.sync="maximum"
+      :products="products"
+      @add="addItem"
+      @delete="deleteItem"
+      @toggle="toggleSliderStatus"
+    ></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// Part 8-1
+// Because router is used from 8-3, I don't need this anymmore.
+// import Products from "./components/Products.vue";
+// Part 8-2
+// Because router is used from 8-3, I don't need this anymmore.
+// import Checkout from "./components/Checkout.vue"
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  name: "app",
+  // Because router is used from 8-3, I don't need this anymmore.
+  // components: {
+  //   Products,
+  //   Checkout
+  // },
+  data: function() {
+    return {
+      maximum: 99,
+      sliderStatus: true,
+      cart: [],
+      products: null
+    };
+  },
+  computed: {
+    cartTotal: function() {
+      let sum = 0;
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function() {
+      let qty = 0;
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    }
+  },
+  methods: {
+    toggleSliderStatus: function() {
+      this.sliderStatus = !this.sliderStatus;
+    },
+    deleteItem: function(id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    },
+    addItem: function(product) {
+      var whichProduct;
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    }
+  },
+  mounted: function() {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then(response => response.json())
+      .then(data => {
+        this.products = data;
+      });
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+// Part 8.3 Router
+import VueRouter from "vue-router";
+// Part 8.3 Router
+import Products from "./components/Products.vue";
+import Checkout from "./components/Checkout.vue";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart, faDollarSign);
+
+// Part 8.3 Router
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
+const router = new VueRouter({
+  routes: [
+    {
+      path: "*",
+      component: Products
+    },
+    {
+      path: "/checkout",
+      component: Checkout
+    }
+  ]
+})
+
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount("#app");
